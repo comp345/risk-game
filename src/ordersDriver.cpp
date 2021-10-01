42e9c8ca04b58c00b5869d3022cf5e5552ee1f61
eyeshield2110 <ndinh@hotmail.fr>
2021-10-01 15:12:16 -0400

    Tested operator= and << for Order and subtypes

--- src/ordersDriver.cpp
+++ src/ordersDriver.cpp
@@ -13,40 +13,50 @@ void testOrdersLink()
 
 void testOrdersDriver()
 {
-
     // Testing creation and init of order and their subtypes here
+    // Test stream insertion operator on Order object and all its subtypes
+    cout << "Testing the stream insertion operators for each Order type/subtype: \n";
+
     Order o1 = Order("First order");
-    cout << o1.getName()
-         << "\n";
+    cout << o1 << "\n";
+
+    Order o2 = Order("Second order");
 
     Deploy deploy1 = Deploy();
-    cout << deploy1.getName()
-         << "\n";
+    cout << deploy1 << "\n";
 
     Advance advance1 = Advance();
-    cout << advance1.getName()
-         << "\n";
+    cout << advance1 << "\n";
 
     Bomb bomb1 = Bomb();
-    cout << bomb1.getName()
-         << "\n";
+    cout << bomb1 << "\n";
 
     Blockade blockade1 = Blockade();
-    cout << blockade1.getName()
-         << "\n";
+    cout << blockade1 << "\n";
 
     AirLift airlift1 = AirLift();
-    cout << airlift1.getName()
-         << "\n";
+    cout << airlift1 << "\n";
 
     Negotiate negotiate1 = Negotiate();
-    cout << negotiate1.getName()
-         << "\n";
+    cout << negotiate1 << "\n";
+
+    // testing the operator=
+    o2 = o1;
+    cout << "\nTesting the assignment operator:\n";
+    cout << "Assigning o1 to o2 => " << "o1: " << o1 << ", o2: " << o2;
+    
+    cout << "\nCheck if assignment created a deep copy (not a shallow copy):\n";
+    o1.setDetails("New details");
+    cout << "o1: " << o1 << ", o2" << o2 ;
+    cout << "\n";
 
     // create OrderList
     OrderList myFirstOrderList;
 
     // testing adding orders (in alphabetical order) to end of list
+    cout << "============================================" 
+         << "\nAdding each type of Order in the OrderList: \n";
+
     myFirstOrderList.add(advance1);
     myFirstOrderList.add(airlift1);
     myFirstOrderList.add(blockade1);
@@ -61,6 +71,8 @@ void testOrdersDriver()
     // myFirstOrderList.add(advance1);
 
     // move orders
+    cout << "============================================" 
+         << "\nTesting moving an order in the list: \n";
     try
     {
         myFirstOrderList.move(0, 5);
@@ -76,6 +88,8 @@ void testOrdersDriver()
     // check invalid moves (OutOfBound)
 
     // remove order by position (starting at position 0)
+    cout << "============================================" 
+         << "\nTesting removing an order from the list: \n";
     myFirstOrderList.remove(4);
     myFirstOrderList.printList();
 }
