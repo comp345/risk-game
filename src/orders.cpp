adb1f89ba090bb3a9ca0863a8358e6cd2526f4bc
eyeshield2110 <ndinh@hotmail.fr>
2021-09-30 23:17:35 -0400

    implemented OrderList's add, move, remove

--- src/orders.cpp
+++ src/orders.cpp
@@ -1,10 +1,88 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include "orders.hpp"
 
 using namespace std;
 
-void testOrdersLink() {
-    cout << "Successfully linked orders.cpp and orders.hpp to main";
+
+// Implementation of Orders
+Order::Order() : name("")
+{
+}
+Order::Order(string orderName) : name(orderName)
+{
+}
+Deploy::Deploy() : Order("Deploy type")
+{
+}
+Deploy::Deploy(string orderName) : Order(orderName)
+{
+}
+Advance::Advance() : Order("Advance type")
+{
+}
+Advance::Advance(string orderName) : Order(orderName)
+{
+}
+Bomb::Bomb() : Order("Bomb type")
+{
+}
+Bomb::Bomb(string orderName) : Order(orderName)
+{
+}
+Blockade::Blockade() : Order("Blockade type")
+{
+}
+Blockade::Blockade(string orderName) : Order(orderName)
+{
+}
+AirLift::AirLift() : Order("Airlift type")
+{
+}
+AirLift::AirLift(string orderName) : Order(orderName)
+{
+}
+Negotiate::Negotiate() : Order("Negotiate type")
+{
+}
+Negotiate::Negotiate(string orderName) : Order(orderName)
+{
 }
 
-// Implementation of Orders list
+// Implementation of OrderList
+OrderList::OrderList()
+{
+    list = new vector<Order>;
+}
+void OrderList::add(Order o)
+{
+    list->push_back(o);
+}
+void OrderList::remove(int i)
+{
+    list->erase(list->begin() + i);
+}
+void OrderList::move(int initPosition, int newPosition)
+{
+    // validate positions (OutOfBound or if init == new)
+    if (initPosition < 0 || initPosition > list->size() - 1 || newPosition < 0 || newPosition > list->size() - 1 || initPosition == newPosition)
+        throw std::out_of_range("Exception from OrderList::move. Invalid position(s).\n");
+
+    // get iterator/node containing the target order
+    Order o = *(list->begin() + initPosition);
+    // erase from the initposition
+    list->erase(list->begin() + initPosition);
+    // insert it back at newposition
+    list->insert(list->begin() + newPosition, o);
+}
+void OrderList::printList()
+{
+    cout << "Printing an order list!";
+    for (vector<Order>::iterator it = list->begin(); it != list->end(); ++it)
+    {
+        cout << " "
+             << (it)->getName();
+    }
+    cout << "\n";
+}
--- src/orders.hpp
+++ src/orders.hpp
@@ -1,49 +1,80 @@
+#include <vector>
+#include <string>
 
 void testOrdersLink();
 
-// implementation 
-
-// OrderList is a DoubleList DA with indexes and nodes 
-class OrderList 
-{
-    // Contains nodes of Orders -> pointers
-    Order *init;
-    
-    public:
-        OrderList();
-        void remove(Order o); 
-        void move(Order o, int i);
-};
 
 // Abstract class (with virtual functions?)
 class Order
 {
+    private:
+        // to delete
+        std::string name;
     public:
-        virtual bool validate();
-        virtual bool execute();
+        Order();
+        Order(std::string name);
+        std::string getName() { return name; }
+        void setName(std::string orderName) { name = orderName; }
+        // To implement
+        //virtual bool validate();
+        //virtual bool execute();
 };
 
-class Deploy : Order 
+class Deploy : public Order 
 {
-    
+    public:
+        Deploy();
+        Deploy(std::string name);
 };
-class Advance : Order 
+class Advance : public Order 
 {
+    public:
+        Advance();
+        Advance(std::string name);
+};
+class Bomb : public Order 
+{
+    public:
+        Bomb();
+        Bomb(std::string name);
+};
+class Blockade : public Order 
+{
+    public:
+        Blockade();
+        Blockade(std::string name);
+};
+class AirLift : public Order 
+{
+    public:
+        AirLift();
+        AirLift(std::string name);
+};
+class Negotiate : public Order 
+{   
+    public:
+        Negotiate();
+        Negotiate(std::string name);
 
 };
-class Bomb : Order 
-{
 
-};
-class Blockade : Order 
-{
 
-};
-class AirLift : Order 
-{
+// OrderList is a DoubleList DA with indexes and nodes 
+class OrderList 
+{   
+    public:
+        OrderList();
+        // adds an order at the end of the orderlist
+        void add(Order o); 
+        void remove(int i);
+        // initPosition and newPosition are positions (0 to size - 1). Throws out_of_range exception.
+        void move(int initPosition, int newPosition);
+        void printList();
+    private:
+        std::vector<Order> *list;
 
+        
 };
-class Negotiate : Order 
-{
 
-};
+// Test function (to delete)
+void testOrdersDriver();
