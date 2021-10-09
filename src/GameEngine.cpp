124134b15de070e3323e1f5b8f6ad35666f3272c
GD-Dheer <gindulink@gmail.com>
2021-10-08 22:43:53 -0400

    more wip

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -1,46 +1,62 @@
-#include "GameEngine.h";
 #include <iostream>
 #include <vector>
 #include <string>
-using namespace GameEngine;
+#include "GameEngine.h"
+//using namespace GameEngine;
 
-StartupPhase::StartupPhase(){
-    // Initialize my variables
+StartupPhase::StartupPhase()
+{
+    // Initialize Engine level variables or env variables..
     currentState = "start";
-    gameStates {
-        {'start', 0},
-        {'loadmap', 1}
-        {'validatemap', 2}
-        {'addplayer,' 3}
-    }
-    void loadMap(string filePath);
-    // Need to pass a map
-    bool mapValidated(Map myMap);
-    // Need to pass some players
+    currentPlayers = 0;
+    // StartupPhase::gameState['start'] = 0;
+    // gameState['loadmap'] = 1;
+    // gameState['validatemap'] = 2;
+    // gameState['addplayer'] = 3;
+
+    startUpLoop();
+    cout << "Got to the consutrcture\n";
+    // //LoadMap should return a Map OBJ (currently has to be a string to test)
+    // string loadMap(string filePath);
+    // // Need to pass a map
+    // bool validatemap(Map myMap);
+    // // Need to pass some players
+}
+
+// Replace with Map function from other parts when ready
+string StartupPhase::loadMap(string myMap)
+{
+    //Assumption that the map is loaded in project directory 
+
+    return "success";
 }
 
 // We can use something like this on next iteration to automate the startup process
-void StartupPhase::StartupPhaseTemplate() 
+void StartupPhase::startUpLoop() 
 {
-    //loadMap() - set default location
+    cout << "Welcome to Warzone. Terminal style!\n";
+    cout << "To start playing, enter the name of the Map you want to load\n";
+    cin >> userInput;
+    mapPath = loadMap(userInput);
+    cout << "To validate map enter: validatemap\n";
+    cin >> userInput
+    validateMap()
+    cout << "To add players enter: addplayer\n";
+    cin >> userInput
+    cout << "Startup Phase complete. Starting the game...\n";
     //validateMap() - auto
     //playerAdd() - manual entry
     //changeState() - enter gameloop
 }
 
-void loadMap(string filePath)
+bool addplayer(Players currentPlayers)
 {
-
+    return true;
 }
 
-bool PlayersAdded(Players currentPlayers)
+bool validatemap(Map myMap)
 {
-
-}
-
-bool mapValidated(Map myMap)
-{
-
+    return true;
 }
 
 void mapUserInputToCommand(string input)
