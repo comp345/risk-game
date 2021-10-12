70a81081f28ab979bf96f77f12c568cbe76ad148
eyeshield2110 <ndinh@hotmail.fr>
2021-10-12 17:32:12 -0400

    fixed redef in gameengine (merge error prob)

--- src/GameEngineDriver.cpp
+++ src/GameEngineDriver.cpp
@@ -2,13 +2,15 @@
 #include <iostream>
 #include <vector>
 #include <string>
+
+using namespace std;
 //using namespace GameEngineSpace;
 
 void gameLoopEngineDriver() 
 {
     GameEngine* engine = new GameEngine();
     engine->run();
-}
+
     cout << "Successfully linked engine.cpp and engine.hpp to main\n";
     GameEngine* tester = new GameEngine();
 }
