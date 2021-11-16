00c43944422e6429477cab858347296134044a67
GD-Dheer <gindulink@gmail.com>
2021-11-15 22:40:01 -0500

    add commandprocessor driver

---
+++ src/CommandProcessorDriver.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string.h>
+#include "GameEngine2.h"
+
+using namespace std;
+
+
+std::string checkCliArgs(int argNumber,char *argv[])
+{
+    string fileName = "";
+    
+    for (int i = 0; i < argNumber; ++i) 
+    {
+        // Not that nice but assumes argv[2] is a file 
+        if(argv[1] && (strcmp(argv[i],"-f")) && argv[2])
+        {
+            fileName = argv[2]; 
+        }
+    }
+    return fileName;
+}
+
+void testCommandProcessor(int argc, char *argv[])
+{
+    string fileName = checkCliArgs(argc, argv);
+    if(fileName.empty())
+    {
+        GameEngine engine;
+        engine.testGameEngine();
+    }
+    else
+    {
+        GameEngine engine{fileName};
+        engine.testGameEngine();
+    }
+}
+
+int main(int argc, char *argv[], char **)
+{
+    testCommandProcessor(argc, argv);
+    return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,15 @@
-#include <iostream>
-#include <string.h>
-#include "Player.h"
-#include "Map.h"
-#include "Orders.hpp"
-#include "Card.h"
-#include "GameEngine2.h"
+// #include <iostream>
+// #include <string.h>
+// #include "Player.h"
+// #include "Map.h"
+// #include "Orders.hpp"
+// #include "Card.h"
+// #include "GameEngine2.h"
 
-using namespace std;
+// using namespace std;
 
-
-std::string checkCliArgs(int argNumber,char *argv[])
-{
-    string fileName = "";
-    
-    for (int i = 0; i < argNumber; ++i) 
-    {
-        // Not that nice but assumes argv[2] is a file 
-        if(argv[1] && (strcmp(argv[i],"-f")) && argv[2])
-        {
-            fileName = argv[2]; 
-        }
-    }
-    return fileName;
-}
-
-void testCommandProcessor(int argc, char *argv[])
-{
-    string fileName = checkCliArgs(argc, argv);
-    if(fileName.empty())
-    {
-        GameEngine engine;
-        engine.testGameEngine();
-    }
-    else
-    {
-        GameEngine engine{fileName};
-        engine.testGameEngine();
-    }
-}
-int main(int argc, char *argv[], char **)
-{
-    testCommandProcessor(argc, argv);
-    return 0;
-}
+// int main(int argc, char *argv[], char **)
+// {
+//     // testCommandProcessor(argc, argv);
+//     // return 0;
+// }
